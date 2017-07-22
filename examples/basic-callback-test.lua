9c5e57f27392e3bbf796939187148c93978086b7
Timidger <APragmaticPlace@gmail.com>
2017-07-21 18:48:43 -0700

    Increased basic callback test coverage
    Added Client and BUtton, later ones will ensure that when a
    register_<callback> is added, coverage is provided by the integration
    test.
    
    Currently, properties and meta table methods aren't tested because they
    aren't implemented.

--- examples/basic-callback-test.lua
+++ examples/basic-callback-test.lua
@@ -1,6 +1,8 @@
 --- Tests basic callback functions for all object types.
 print("Starting basic callback test")
 
+-- TODO Test properties
+
 -- Test awesome functions
 awesome.quit()
 awesome.exec()
@@ -24,5 +26,62 @@ awesome.xrdb_get_value()
 awesome.kill()
 awesome.sync()
 
+-- Test button functions
+button.add_signal()
+button.connect_signal()
+button.disconnect_signal()
+button.emit_signal()
+button.instances()
+button.set_index_miss_handler()
+button.set_newindex_miss_handler()
+button.__call()
+
+-- Test button properties
+button.button()
+button.modifiers()
+
+-- Test button meta functions
+-- TODO Lib doesn't add meta functions yet
+--button.__metatable.__tostring()
+--button.__metatable.__index()
+--button.__metatable.__newindex()
+--button.__metatable.disconnect_signal()
+--button.__metatable.connect_signal()
+
+-- Test client functions
+client.add_signal()
+client.connect_signal()
+client.disconnect_signal()
+client.emit_signal()
+client.instances()
+client.set_index_miss_handler()
+client.set_newindex_miss_handler()
+client.__call()
+client.__get()
+client.__index()
+client.__newindex()
+client.keys()
+client.isvisible()
+client.geometry()
+client.apply_size_hints()
+client.tags()
+client.kill()
+client.swap()
+client.raise()
+client.lower()
+client.unmanange()
+client.titlebar_top()
+client.titlebar_right()
+client.titlebar_bottom()
+client.titlebar_left()
+client.get_icon()
+
+-- Test client meta functions
+-- TODO Lib doesn't add meta functions yet
+--client.__metatable.__tostring()
+--client.__metatable.connect_signal()
+--client.__metatable.disconnect_signal()
+--client.__metatable.__index()
+--client.__metatable.__newindex()
 
 print("Basic callback test complete")
