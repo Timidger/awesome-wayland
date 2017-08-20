0d69f1ddd33d3a873357f7f42b174a4a2f9c52a2
Timidger <APragmaticPlace@gmail.com>
2017-08-19 17:32:07 -0700

    Implemented Button methods, move things around
    The way it is now, there's no reason for the user to implement any of
    the methods on button...so that's interesting

--- src/callbacks/button.rs
+++ src/callbacks/button.rs
@@ -6,6 +6,18 @@ use ::object::signal::Signal;
 use libc::c_int;
 use xcb::ffi::xproto::xcb_button_t;
 
+LUA_OBJECT_FUNCS!(luaA::BUTTON_CLASS, Class, button_new);
+LUA_CLASS_FUNCS!(luaA::BUTTON_CLASS,
+                 button_class_add_signal,
+                 button_class_connect_signal,
+                 button_class_disconnect_signal,
+                 button_class_emit_signal,
+                 button_class_instances,
+                 button_set_index_miss_handler,
+                 button_set_newindex_miss_handler);
+LUA_OBJECT_EXPORT_PROPERTY!(button_get_button, ButtonState, button, lua_pushinteger);
+LUA_OBJECT_EXPORT_PROPERTY!(button_get_modifiers, ButtonState, button, pushmodifiers);
+
 /// State of the button
 #[repr(C)]
 pub struct ButtonState {
@@ -17,14 +29,46 @@ pub struct ButtonState {
 #[allow(non_snake_case)]
 pub trait Button {
     /* Methods */
-    fn button_add_signal(&self, lua: Lua) -> c_int;
-    fn button_connect_signal(&self, lua: Lua) -> c_int;
-    fn button_disconnect_signal(&self, lua: Lua) -> c_int;
-    fn button_emit_signal(&self, lua: Lua) -> c_int;
-    fn button_instances(&self, lua: Lua) -> c_int;
-    fn button_set_index_miss_handler(&self, lua: Lua) -> c_int;
-    fn button_set_newindex_miss_handler(&self, lua: Lua) -> c_int;
-    fn button___call(&self, lua: Lua) -> c_int;
+    fn button_add_signal(&self, lua: Lua) -> c_int {
+        unsafe {
+            button_class_add_signal(lua.0)
+        }
+    }
+    fn button_connect_signal(&self, lua: Lua) -> c_int {
+        unsafe {
+            button_class_connect_signal(lua.0)
+        }
+    }
+    fn button_disconnect_signal(&self, lua: Lua) -> c_int {
+        unsafe {
+            button_class_disconnect_signal(lua.0)
+        }
+    }
+    fn button_emit_signal(&self, lua: Lua) -> c_int {
+        unsafe {
+            button_class_emit_signal(lua.0)
+        }
+    }
+    fn button_instances(&self, lua: Lua) -> c_int {
+        unsafe {
+            button_class_instances(lua.0)
+        }
+    }
+    fn button_set_index_miss_handler(&self, lua: Lua) -> c_int {
+        unsafe {
+            button_set_index_miss_handler(lua.0)
+        }
+    }
+    fn button_set_newindex_miss_handler(&self, lua: Lua) -> c_int {
+        unsafe {
+            button_set_newindex_miss_handler(lua.0)
+        }
+    }
+    fn button___call(&self, lua: Lua) -> c_int {
+        unsafe {
+            luaA::button_new(lua.0)
+        }
+    }
     /* Meta */
     fn button___tostring_meta(&self, lua: Lua) -> c_int {
         unsafe {
@@ -41,6 +85,13 @@ pub trait Button {
             luaA::object_disconnect_signal_simple(lua.0)
         }
     }
+
+    fn button_emit_signal_meta(&self, lua: Lua) -> c_int {
+        unsafe {
+            luaA::object_emit_signal_simple(lua.0)
+        }
+    }
+
     /* LUA_CLASS_META methods */
     fn button___index_meta(&self, lua: Lua) -> c_int {
         unsafe {
@@ -52,30 +103,13 @@ pub trait Button {
             luaA::class_newindex(lua.0)
         }
     }
-    /* Properties  */
-    properties!([
-        button,
-        modifiers
-    ]);
 }
 
 use luaA::pushmodifiers;
-LUA_OBJECT_EXPORT_PROPERTY!(button_get_button, ButtonState, button, lua_pushinteger);
-LUA_OBJECT_EXPORT_PROPERTY!(button_get_modifiers, ButtonState, button, pushmodifiers);
 
 use lua_sys::*;
 use ::object::class::{Class, Object};
-
 pub unsafe fn button_class_setup(lua: *mut lua_State) {
-    LUA_OBJECT_FUNCS!(luaA::BUTTON_CLASS, Class, button_new);
-    LUA_CLASS_FUNCS!(luaA::BUTTON_CLASS,
-                     button_class_add_signal,
-                     button_class_connect_signal,
-                     button_class_disconnect_signal,
-                     button_class_emit_signal,
-                     button_class_instances,
-                     button_set_index_miss_handler,
-                     button_set_newindex_miss_handler);
     let button_methods = [
         luaL_Reg {
             name: c_str!("add_signal"),
@@ -160,3 +194,4 @@ pub unsafe fn button_class_setup(lua: *mut lua_State) {
                              Some(button_get_modifiers),
                              Some(luaA::button_set_modifiers));
 }
+
