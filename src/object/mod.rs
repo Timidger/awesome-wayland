c95234400b3f040a5097575544c5f80ab6aa6b40
Timidger <APragmaticPlace@gmail.com>
2017-08-20 13:28:50 -0700

    Added WindowState
    Haven't added it as a proper object yet, and haven't added the trait
    object (Window) exposed to the user quite yet either)

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,6 +1,8 @@
 pub mod class;
 pub mod property;
 pub mod signal;
+pub mod window;
 
 pub use self::property::Property;
 pub use self::signal::{Signal, GLOBAL_SIGNALS, signal_object_emit};
+pub use self::window::Window;
---
+++ src/object/window.rs
@@ -0,0 +1,53 @@
+//! Definition for the Window class. This class is a super class to various
+//! objects, including drawable.
+
+use ::luaA::{self, pushmodifiers};
+use ::lua::Lua;
+use ::object::signal::Signal;
+use ::object::class::{Class, Object};
+use libc::{c_void, c_int};
+use lua_sys::*;
+use xcb::ffi::xproto::xcb_button_t;
+use ::callbacks::button::ButtonState;
+
+// TODO Remove this lint
+#[allow(unused_variables)]
+#[repr(C)]
+pub struct WindowState {
+    pub signals: Vec<Signal>,
+    /// The X window number
+    pub window: i32,
+    /// The frame window, might be XCB_NONE
+    pub frame_window: (),
+    /// Opacity
+    pub opacity: f64,
+    /// Struct
+    pub strut: Struct,
+    /// Button bindings
+    pub buttons: Vec<*mut ButtonState>,
+    /// Do we have pending border changes?
+    pub border_need_update: bool,
+    /// Border color
+    pub border_color: (),
+    /// The window type
+    pub window_type: (),
+    /// The border width callback
+    pub border_width_callback: fn(*mut c_void, u16, u16)
+}
+
+
+#[repr(C)]
+pub struct Struct {
+    pub left: u16,
+    pub right: u16,
+    pub top: u16,
+    pub bottom: u16,
+    pub left_start_y: u16,
+    pub left_end_y: u16,
+    pub right_start_y: u16,
+    pub right_end_y: u16,
+    pub top_start_x: u16,
+    pub top_end_x: u16,
+    pub bottom_start_x: u16,
+    pub bottom_end_x: u16
+}
