ad7e495da9ca23a9b473e1392045c22705d756d3
Timidger <APragmaticPlace@gmail.com>
2017-08-19 18:04:31 -0700

    Removed unused button_class_setup

--- src/callbacks/button.rs
+++ src/callbacks/button.rs
@@ -1,10 +1,13 @@
 //! Callbacks for the `button` object in the Lua libraries
 
-use ::luaA;
+use ::luaA::{self, pushmodifiers};
 use ::lua::Lua;
 use ::object::signal::Signal;
+use ::object::class::{Class, Object};
 use libc::c_int;
+use lua_sys::*;
 use xcb::ffi::xproto::xcb_button_t;
+
 
 LUA_OBJECT_FUNCS!(luaA::BUTTON_CLASS, Class, button_new);
 LUA_CLASS_FUNCS!(luaA::BUTTON_CLASS,
@@ -104,94 +107,3 @@ pub trait Button {
         }
     }
 }
-
-use luaA::pushmodifiers;
-
-use lua_sys::*;
-use ::object::class::{Class, Object};
-pub unsafe fn button_class_setup(lua: *mut lua_State) {
-    let button_methods = [
-        luaL_Reg {
-            name: c_str!("add_signal"),
-            func: Some(button_class_add_signal)
-        },
-        luaL_Reg {
-            name: c_str!("connect_signal"),
-            func: Some(button_class_connect_signal)
-        },
-        luaL_Reg {
-            name: c_str!("disconnect_signal"),
-            func: Some(button_class_disconnect_signal)
-        },
-        luaL_Reg {
-            name: c_str!("emit_signal"),
-            func: Some(button_class_emit_signal)
-        },
-        luaL_Reg {
-            name: c_str!("instances"),
-            func: Some(button_class_instances)
-        },
-        luaL_Reg {
-            name: c_str!("set_index_miss_handler"),
-            func: Some(button_set_index_miss_handler)
-        },
-        luaL_Reg {
-            name: c_str!("set_newindex_miss_handler"),
-            func: Some(button_set_newindex_miss_handler)
-        },
-        luaL_Reg {
-            name: c_str!("__call"),
-            func: Some(luaA::button_new)
-        },
-        luaL_Reg {
-            name: ::std::ptr::null_mut(),
-            func: None
-        }
-    ];
-
-    let button_meta = [
-        luaL_Reg {
-            name: c_str!("__tostring"),
-            func: Some(luaA::object_tostring)
-        },
-        luaL_Reg {
-            name: c_str!("connect_signal"),
-            func: Some(luaA::object_connect_signal_simple)
-        },
-        luaL_Reg {
-            name: c_str!("disconnect_signal"),
-            func: Some(luaA::object_disconnect_signal_simple)
-        },
-        luaL_Reg {
-            name: c_str!("emit_signal"),
-            func: Some(luaA::object_emit_signal_simple)
-        },
-        luaL_Reg {
-            name: c_str!("__index"),
-            func: Some(luaA::class_index)
-        },
-        luaL_Reg {
-            name: c_str!("__newindex"),
-            func: Some(luaA::class_newindex)
-        },
-        luaL_Reg {
-            name: ::std::ptr::null_mut(),
-            func: None
-        }
-    ];
-    let mut button_class = luaA::BUTTON_CLASS.try_write().unwrap();
-    luaA::class_setup(lua, &mut *button_class, c_str!("button"), null_mut() as _,
-                      button_new, None, None,
-                      Some(luaA::class_index_miss_property),
-                      Some(luaA::class_newindex_miss_property),
-                      &button_methods, &button_meta);
-    luaA::class_add_property(&mut *button_class, "button",
-                             Some(luaA::button_set_button),
-                             Some(button_get_button),
-                             Some(luaA::button_set_button));
-    luaA::class_add_property(&mut *button_class, "modifiers",
-                             Some(luaA::button_set_modifiers),
-                             Some(button_get_modifiers),
-                             Some(luaA::button_set_modifiers));
-}
-
